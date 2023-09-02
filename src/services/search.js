287d9514aeed71fb7a5e709105ebc41bfab90f2d
asmindev <asmin987asmin@gmail.com>
2023-09-02 21:10:47 +0800

    fix bug

--- src/services/search.js
+++ src/services/search.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import quran from "./quran.json";
-import { getCloseMatches } from "difflib";
 
 const useQuranSearch = (query) => {
     const [searchResults, setSearchResults] = useState([]);
