dcb2a1c660b7d3135569fb45c7fc317311a15703
asmindev <asmin987asmin@gmail.com>
2023-09-02 21:05:00 +0800

    fix bug

---
+++ src/services/quran.config.js
@@ -0,0 +1,7 @@
+const defaultConfig = {
+    language: "id",
+    audio: 7,
+    word_fields: "text_uthmani",
+};
+
+export default defaultConfig;
---
+++ src/services/search.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+import quran from "./quran.json";
+import { getCloseMatches } from "difflib";
+
+const useQuranSearch = (query) => {
+    const [searchResults, setSearchResults] = useState([]);
+
+    useEffect(() => {
+        if (query) {
+            const nama = quran.chapters.filter((item) =>
+                item.name_simple.toLowerCase().startsWith(query.toLowerCase())
+            );
+            setSearchResults(nama);
+        }
+    }, [query]);
+
+    return searchResults;
+};
+
+export default useQuranSearch;
